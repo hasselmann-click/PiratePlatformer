f23933532056651f39bc71a6b27aa9cd18c2d83b
radall <radall@outlook.com>
2024-12-30 20:26:27 +0100

    build container v1

--- export-android.sh
+++ export-android.sh
@@ -4,18 +4,16 @@ echo "Exporting project to Android APK"
 
 # Image to use
 IMAGE="godot-android-build-env"
-
 # Set the project paths
-PROJECT_PATH="."
+PROJECT_PATH=$(pwd)
 EXPORT_PATH="${PROJECT_PATH}/exports"
-
 # Set the path to the export templates
 # download them from https://godotengine.org/download/linux/
-# TEMPLATES_PATH="./exports/templates"  
-# if [ ! -d "$TEMPLATES_PATH" ]; then
-#     echo "Export templates directory does not exist: $TEMPLATES_PATH"
-#     exit 1
-# fi
+TEMPLATES_PATH="${PROJECT_PATH}/exports/templates/android"  
+if [ ! -d "$TEMPLATES_PATH" ]; then
+    echo "Export templates directory does not exist: $TEMPLATES_PATH"
+    exit 1
+fi
 
 echo "Building Docker image: $IMAGE"
 # Run the Docker container to export the project
@@ -25,13 +23,14 @@ echo "Running Docker container: $IMAGE"
 echo "Exporting to: $EXPORT_PATH"
 echo "Project path: $PROJECT_PATH"
 
+# Remark: no-editor-plugins (o.s.) will be relevant in the future: https://github.com/godotengine/godot-proposals/issues/2628
 docker run --rm \
     -v "${PROJECT_PATH}:/workdir" \
     -v "${EXPORT_PATH}:/workdir/exports" \
+    -v "${TEMPLATES_PATH}:/workdir/exports/templates/android" \
     -w /workdir \
     $IMAGE --verbose --headless \
-        --export-debug "Android" "/workdir/exports/my_game.apk"
-        #--import
-        # -v "$TEMPLATES_PATH:/root/.local/share/godot/templates/4.3.stable" \
+        --no-editor-plugins \
+        --export-debug "Android"
 
 echo "Done"
